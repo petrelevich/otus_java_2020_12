481a31c96eada2fdf7afde3377262ce31f7391f7
Sergey Petrelevich <petrelevich@yandex.ru>
2021-01-02 16:27:16 +0300

    hw03-collections

---
+++ HW03-collections/src/main/java/ru/otus/CustomerReverseOrder.java
@@ -0,0 +1,15 @@
+package ru.otus;
+
+
+public class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+
+    public void add(Customer customer) {
+
+    }
+
+    public Customer take() {
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+}
---
+++ HW03-collections/src/main/java/ru/otus/CustomerService.java
@@ -0,0 +1,25 @@
+package ru.otus;
+
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class CustomerService {
+
+    //todo: 3. надо реализовать методы этого класса
+
+    public Map.Entry<Customer, String> getSmallest() {
+        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        return null; // это "заглушка, чтобы скомилировать"
+    }
+
+    public void add(Customer customer, String data) {
+
+    }
+}
