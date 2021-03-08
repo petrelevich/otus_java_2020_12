3caff8aed4ded48b7e1a3268c2c8c67cf4db4c5f
Sergey Petrelevich <petrelevich@yandex.ru>
2021-03-08 18:40:30 +0300

    L16 io

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -4,15 +4,12 @@ import ru.otus.model.Measurement;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
         //группирует выходящий список по name, при этом суммирует поля value
-
-        return data.stream()
-                .collect(Collectors.groupingBy(Measurement::getName, Collectors.summingDouble(Measurement::getValue)));
+        return null;
     }
 }
--- L16-io/homework/src/test/java/ru/otus/dataprocessor/ProcessorTest.java
+++ L16-io/homework/src/test/java/ru/otus/dataprocessor/ProcessorTest.java
@@ -18,11 +18,9 @@ class ProcessorTest {
     //Надо реализовать методы классов и убедиться, что тест проходит
 
     @Test
-    //@Disabled // Эту аннотацию надо убрать
+    @Disabled // Эту аннотацию надо убрать
     @DisplayName("Из файла читается json, обрабатывается, результат сериализуется в строку")
     void processingTest(@TempDir Path tempDir) throws IOException {
-
-
         System.out.println(tempDir);
 
         //given
