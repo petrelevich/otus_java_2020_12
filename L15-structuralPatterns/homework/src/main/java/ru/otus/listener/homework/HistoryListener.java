15e96936413f9189f4b7063a598c2087b42eff9c
Sergey Petrelevich <petrelevich@yandex.ru>
2021-03-14 16:58:43 +0300

    L15 updated homework

---
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -0,0 +1,19 @@
+package ru.otus.listener.homework;
+
+import ru.otus.listener.Listener;
+import ru.otus.model.Message;
+
+import java.util.Optional;
+
+public class HistoryListener implements Listener, HistoryReader {
+
+    @Override
+    public void onUpdated(Message oldMsg, Message newMsg) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<Message> findMessageById(long id) {
+        throw new UnsupportedOperationException();
+    }
+}
