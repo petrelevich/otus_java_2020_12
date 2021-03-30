f3c6f0ad1e9bbb7ab03904cd1d12c173f08df076
Sergey Petrelevich <petrelevich@yandex.ru>
2021-03-30 23:03:58 +0300

    L22 cache

--- L22-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
+++ L22-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
@@ -15,21 +15,21 @@ public class HWCacheDemo {
     }
 
     private void demo() {
-        HwCache<Integer, Integer> cache = new MyCache<>();
+        HwCache<String, Integer> cache = new MyCache<>();
 
         // пример, когда Idea предлагает упростить код, при этом может появиться "спец"-эффект
-        HwListener<Integer, Integer> listener = new HwListener<Integer, Integer>() {
+        HwListener<String, Integer> listener = new HwListener<String, Integer>() {
             @Override
-            public void notify(Integer key, Integer value, String action) {
+            public void notify(String key, Integer value, String action) {
                 logger.info("key:{}, value:{}, action: {}", key, value, action);
             }
         };
 
         cache.addListener(listener);
-        cache.put(1, 1);
+        cache.put("1", 1);
 
-        logger.info("getValue:{}", cache.get(1));
-        cache.remove(1);
+        logger.info("getValue:{}", cache.get("1"));
+        cache.remove("1");
         cache.removeListener(listener);
     }
 }
